2b860e3399d5a85ea50d52bbe7189b7515054441
Ernesto <benjamin.vazquez@iteso.mx>
2023-09-21 14:50:40 -0600

    adding code

---
+++ src/modules/establecimientos/establecimientos.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const establecimientoController = require('./establecimientoController');
+
+router.put('/:id', establecimientoController.agregarMesa); // UPDATE mesa
+
+router.put('/:id', establecimientoController.eliminarMesa); // UPDATE mesa
+
+router.put('/:id', establecimientoController.agregarEmpleado); // UPDATE empleado
+
+router.put('/:id', establecimientoController.eliminarEmpleado); // UPDATE empleado
+
+router.put('/:id', establecimientoController.agregarMesero); // UPDATE meseros
+
+router.put('/:id', establecimientoController.agregarMesero); // UPDATE meseros
+
+module.exports = router;
